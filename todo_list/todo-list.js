18d889974f5ff01046182f910c7f4e0451940b59
Vijay-eshwaran <vijay.eshwaran37@gmail.com>
2024-12-26 19:52:38 +0530

    version 3 final product..?

--- todo_list/todo-list.js
+++ todo_list/todo-list.js
@@ -1,47 +1,81 @@
+const nameInput = document.querySelector(".name-input");
+const dateInput = document.querySelector(".date-input");
+const addBtn = document.querySelector(".add-btn");
+const list = document.querySelector(".list");
+const deleteBtn = document.querySelector(".delete-btn");
+
+
+
 const todolist = [];
 
+
+
 function renderTodoList() {
-
   let todohtml = '';
-
-  for(let i=0;i<todolist.length;i++){
-    const todo = todolist[i];
-    const html = `<p>${todo}</p>`
+  for (let i = 0; i < todolist.length; i++) {
+    const todoobj = todolist[i];
+    // const name = todoobj.title;
+    // const date = todoobj.date;
+    const {name} = todoobj;
+    const {date} = todoobj;
+    const html = `
+    <div class="todoname">${name}</div>  
+    <div class="tododate">${date}</div>
+    <button class="delete-btn" onclick="deleteTodoList(${i})">Delete</button>`;
     todohtml += html;
   }
-  
   console.log(todohtml);
   document.querySelector('.list').innerHTML = todohtml;
 }
 
+function deleteTodoList(index) {
+  todolist.splice(index, 1);
+  renderTodoList();
+  if(list.innerHTML === ''){
+    list.innerHTML = 'Empty :)'
+  }
+}
 
-const input = document.querySelector(".input");
-const addBtn = document.querySelector(".add-btn");
-const list = document.querySelector(".list");
+
+
 
 addBtn.addEventListener("click", function () {
-  if (input.value === "") {
-    input.placeholder = "Please enter a task !!!!!";
+  if (nameInput.value === "" ) {
+    nameInput.placeholder = "Please enter a task !!!!!";
     return;
   }
+  if (dateInput.value === "") {
+    dateInput.value = "No date";
+  } 
 
+  const obj = {
+    name: nameInput.value,
+    date: dateInput.value
+  }
 
-  todolist.push(input.value);
-  input.value = "";
-
+  todolist.push(obj);
+  nameInput.value = "";
+  dateInput.value = "";
 
   console.log(todolist);
   renderTodoList();
+
 });
 
-input.addEventListener("keydown", function (event) {
+nameInput.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
-    if (input.value === "") {
-      input.placeholder = "Please enter a task !!!!!";
+    if (nameInput.value === "") {
+      nameInput.placeholder = "Please enter a task !!!!!";
       return;
     }
+    
 
-    todolist.push(input.value);
+    const obj = {
+      name: nameInput.value,
+      date: dateInput.value
+    }
+  
+    todolist.push(obj);
     input.value = "";
 
     console.log(todolist);
