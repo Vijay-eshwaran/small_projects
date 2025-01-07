60405cee3789d463c5e11ecd7d668a4ff09aaed5
Vijay-eshwaran <vijay.eshwaran37@gmail.com>
2025-01-07 07:07:41 +0530

    added amazon project

---
+++ amazon/scripts/amazon.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+        rating: {
+            stars: 4.5,
+            count: 87
+        },
+        price: 80,
+    },
+    {
+        image: 'images/products/intermediate-composite-basketball.jpg',
+        name: 'Intermediate Size Basketball',
+        rating: {
+            rating: 4.0,
+            count: 127
+        },
+        price: 280
+    },
+    {
+        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+        rating: {
+            rating: 4.5,
+            count: 45
+        },
+        price: 400
+    }
+];
+
+console.log(products);
