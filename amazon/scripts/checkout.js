571a14526ff1b369abb2c8240e0bb135a464c27a
Vijay-eshwaran <vijay.eshwaran37@gmail.com>
2025-01-23 19:07:00 +0530

    added backend practise file

--- amazon/scripts/checkout.js
+++ amazon/scripts/checkout.js
@@ -1,5 +1,6 @@
 import { rendorOrderSummary } from "./checkout/orderSummary.js";
-import { rendorPaymentSummary } from "./checkout/paymentSummary.js";    
+import { rendorPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/backend-practise.js';
 // import '../data/cart-class.js'
 
 rendorOrderSummary();
