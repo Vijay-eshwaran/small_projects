f94476b0bc4a90dbcab29528d36d05a3e4bd0853
Vijay-eshwaran <vijay.eshwaran37@gmail.com>
2025-01-23 21:38:39 +0530

    ch

--- amazon/scripts/checkout.js
+++ amazon/scripts/checkout.js
@@ -1,19 +1,7 @@
 import { rendorOrderSummary } from "./checkout/orderSummary.js";
 import { rendorPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
-// import '../data/backend-practise.js';
 // import '../data/cart-class.js'
 
-new Promise( (resolve) => {
-    loadProducts( () => {
-        resolve();
-    });
-}).then( () => {
-    rendorOrderSummary();
-    rendorPaymentSummary();
-})
 
-// loadProducts(() => {
-//     rendorOrderSummary();
-//     rendorPaymentSummary();
-// });
+rendorOrderSummary();
+rendorPaymentSummary();
